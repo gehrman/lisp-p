3a458e427a67fe5987b0ce9fabf1ada8424bc037
Geoff Ehrman <gehrman@gmail.com>
2016-01-21 22:33:13 -0500

    Stubs for windows.
    I feel dirty using ifdef like that.

--- src/repl.c
+++ src/repl.c
@@ -1,13 +1,37 @@
 /*
 IDEA: command count in prompt + unsaved keystroke to clear count
+
+TODO: Replace libedit with linenoise once things are working, and
+      I'm a a little more familiar with C.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Replace libedit with linenoise once things are working, and I'm a
-   a little more familiar with C. */
+/* there has to be a better way to do cross-platform work...
+   this seems vile */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline on windows. */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) - 1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history on windows. */
+void add_history(char* unused) {}
+
+/* not windows means real libedit */
+#else
 #include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
   puts("LispP Version 0.0");
