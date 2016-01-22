71fb103c8adc6d55df97749e737a646999e7d42d
Geoff Ehrman <gehrman@gmail.com>
2016-01-21 21:40:09 -0500

    No history.h needed on a mac.
    Also, twiddle some spacing.

--- src/repl.c
+++ src/repl.c
@@ -8,10 +8,9 @@ IDEA: command count in prompt + unsaved keystroke to clear count
 /* Replace libedit with linenoise once things are working, and I'm a
    a little more familiar with C. */
 #include <editline/readline.h>
-#include <editline/history.h>
 
 int main(int argc, char** argv) {
-  puts("LispP Version 0.0\n");
+  puts("LispP Version 0.0");
   puts("Ctrl-C to Exit.\n");
 
   /* REPL main loop. Strictly speaking, not yet a REP loop yet so much
@@ -27,7 +26,7 @@ int main(int argc, char** argv) {
     add_history(input);
 
     /* Like I said, no actual E in the REPL yet. */
-    printf("No you're a towel. Now where's my %s?\n", input);
+    printf("No you're a towel. Now where's my %s?\n\n", input);
 
     /* We're done with input, so it's time to free it. */
     free(input);
